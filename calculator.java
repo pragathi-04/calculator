34639e09d23bce939bf9bda8415121c840f0c85f
Pragathi <gpragathi04@gmail.com>
2025-12-23 14:09:40 +0530

    Add Calculator Java program

---
+++ calculator.java
@@ -0,0 +1,8 @@
+public class calculator {
+    public static void main(String[] args) {
+        int a=10,b=6;
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Multiplication: " + (a * b));
+    }
+    
+}
